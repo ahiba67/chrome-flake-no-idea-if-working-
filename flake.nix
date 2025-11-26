d1d886262fd162be32cee341a59deb5bee7cdeb3
ahiba67 <floppyuh@gmail.com>
2025-11-26 19:57:56 +0200

    Add files via upload

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+ description = "flake of somekind"
+
+ inputs = {
+ nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"
+ };
+
+ outputs = { self, nixpkgs}
+
+let
+pkgs = import nixpkgs {
+ system = "x86_64-linux";
+ config.allowUnfree = true;
+};
+in
+{
+    devshells.default = pkgs.mkShell {
+        buildInputs = [
+            pkgs.google-chrome 
+            ]
+          };
+        };
+     }
+        
+    
