3795cff9f51d800ff5c69e64d1ee295e227d990e
ahiba67 <floppyuh@gmail.com>
2025-11-26 21:16:08 +0200

    Add files via upload

--- flake.nix
+++ flake.nix
@@ -1,17 +1,19 @@
 {
- description = "flake of somekind"
+ description = "flake of somekind";
 
  inputs = {
- nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"
+  nixpkgs = {
+     url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
  };
 
- outputs = { self, nixpkgs}
+ outputs = { self, nixpkgs }:
 
 let
 pkgs = import nixpkgs {
  system = "x86_64-linux";
  config.allowUnfree = true;
-};
+  };
 in
 {
     devshells.default = pkgs.mkShell {
